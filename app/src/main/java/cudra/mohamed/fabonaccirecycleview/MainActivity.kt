a848cdf44c29c919faec1cdf94c1ea8db9882b56
CudraMohamed <ncudramohamed@gmail.com>
2022-07-03 21:59:22 +0300

    removed comments

--- app/src/main/java/cudra/mohamed/fabonaccirecycleview/MainActivity.kt
+++ app/src/main/java/cudra/mohamed/fabonaccirecycleview/MainActivity.kt
@@ -13,27 +13,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        fabonacci()
 
+        fabonacci()
     }
     fun fabonacci (){
-        val end = 100
         var num1=0
         var num2=1
 
-        print("First $end terms: ")
         var numbList= mutableListOf<Int>()
-        for (num in 1..end){
-            numbList.add(num1)
-//            print("$num1 + ")
+        for (num in 1..100){
 
+            numbList.add(num1)
             var sum=num1+num2
             num1=num2
             num2=sum
 
-            var numberAdapter=NumberRecyclerViewAdapter(numbList)
-            binding.rvNumbers.layoutManager = LinearLayoutManager(this)
-            binding.rvNumbers.adapter = numberAdapter
         }
+        var numberAdapter=NumberRecyclerViewAdapter(numbList)
+        binding.rvNumbers.layoutManager = LinearLayoutManager(this)
+        binding.rvNumbers.adapter = numberAdapter
     }
 }
